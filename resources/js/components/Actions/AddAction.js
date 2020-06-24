57111a21f4a49cac80117b5a41ea963933ee6a49
Cse_academia <62127870+CSEacademia@users.noreply.github.com>
2020-06-25 00:57:08 +0600

    url change

--- resources/js/components/Actions/AddAction.js
+++ resources/js/components/Actions/AddAction.js
@@ -4,7 +4,7 @@ const AddAction=(data)=> async dispatch=>{
   
     try {
        
-       const res= await axios.post(`https://kb-rrl.herokuapp.com/api/get/`,data); 
+       const res= await axios.post(`https://kb-rrl.herokuapp.com/api/get`,data); 
         dispatch({type:"ADD",data:data});
     } catch (error) {
                  
--- resources/js/components/Actions/DeleteAction.js
+++ resources/js/components/Actions/DeleteAction.js
@@ -4,7 +4,7 @@ const DeleteAction=(id)=> async dispatch=>{
    
     try {
        
-       const res= await axios.delete(`https://kb-rrl.herokuapp.com/${id}`); 
+       const res= await axios.delete(`https://kb-rrl.herokuapp.com/api/get/${id}`); 
         dispatch({type:"DELETE",payload:id});
     } catch (error) {
                  
