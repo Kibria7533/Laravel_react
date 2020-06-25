8babaafa50f2989107b78ea681193777c2873057
Cse_academia <62127870+CSEacademia@users.noreply.github.com>
2020-06-25 14:23:13 +0600

    last

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
-        <title>Laravel</title>
+        <title>KB-RRL</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
